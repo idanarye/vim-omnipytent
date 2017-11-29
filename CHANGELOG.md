b6231c88680edd7e60379be47c43781f151f2932
IdanArye <idanarye@gmail.com>
2017-11-30 00:04:19 +0200

    Support Plumbum's `with_env()`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Windows support.
 - `TERMINAL_PANEL` and `TERMINAL_TAB` support for Vim 8.
 - `omnipytent.ext` for loading module from `omnipytent` directories in Vim plugins.
+- Support Plumbum's `with_env()`(Linux+Windows)
 
 ## 0.1.0 - 2016-08-26
 ### Added
--- autoload/omnipytent/integration/plumbum.py
+++ autoload/omnipytent/integration/plumbum.py
@@ -1,11 +1,38 @@
 from __future__ import absolute_import
 
-from plumbum import local
+from itertools import chain
 
-from omnipytent.execution import ShellCommandExecuter
+import vim
+
+from plumbum import local  # not used here, but users are expected to import it
+
+from omnipytent.execution import ShellCommandExecuter, quote
+
+
+if int(vim.eval('has("win32")')):
+    def __format_env_vars(cmd):
+        try:
+            env_vars = cmd.envvars
+        except AttributeError:
+            return
+
+        yield 'cmd'
+        yield '/c'
+        yield quote(''.join('set %s=%s&&' % pair for pair in env_vars.items()))
+else:
+    def __format_env_vars(cmd):
+        try:
+            env_vars = cmd.envvars
+        except AttributeError:
+            return
+
+        for k, v in env_vars.items():
+            yield '%s=%s' % (k, quote(v))
 
 
 def __rand__(self, cmd):
-    return self(*map(ShellCommandExecuter.Raw, cmd.formulate(1)))
-ShellCommandExecuter.__rand__ = __rand__
+    return self(*map(ShellCommandExecuter.Raw, chain(__format_env_vars(cmd),
+                                                     cmd.formulate(1))))
 
+
+ShellCommandExecuter.__rand__ = __rand__
